fc0c911a1014860639c1a76570c8d4d728acffc7
Sogurung16 <Sogurung16@gmail.com>
2021-08-20 04:41:59 +0100

    SQLQueries implemented for select all, record, delete record and update record

--- src/main/java/com/sparta/sonam/employeecsvproject/controller/jbdc/DatabaseWriter.java
+++ src/main/java/com/sparta/sonam/employeecsvproject/controller/jbdc/DatabaseWriter.java
@@ -5,17 +5,19 @@ import org.apache.log4j.Logger;
 
 import java.util.Collection;
 import java.util.HashMap;
+import java.util.Iterator;
 
 public class DatabaseWriter implements Runnable{
     private static Logger logger = Logger.getLogger("DatabaseWriter");
     public static void writeOntoDB(HashMap<Integer, EmployeeDTO> employeeDTOHashMap){
         EmployeeDAO employeeDAO = new EmployeeDAO(ConnectionManager.connectToDB());
         Collection<EmployeeDTO> employeeDTOCollection = employeeDTOHashMap.values();
-        logger.info(employeeDTOHashMap.get(198429));
+        /*logger.info(employeeDTOHashMap.get(198429));
         employeeDAO.createRecord(employeeDTOHashMap.get(198429));
-        /*for(EmployeeDTO employeeDTO: employeeDTOCollection){
+        EmployeeDTO employeeDTO;*/
+        for(EmployeeDTO employeeDTO: employeeDTOCollection){
             employeeDAO.createRecord(employeeDTO);
-        }*/
+        }
     }
 
     @Override
